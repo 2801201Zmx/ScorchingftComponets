299df235effc0ac7501cf104427de1fc6cafe80f
幻想Gennsou <2801206692@qq.com>
2025-08-05 19:00:10 +0800

    暂时先“组件开发中，敬请期待”和加载动画

--- src/views/components/overview.vue
+++ src/views/components/overview.vue
@@ -1,9 +1,27 @@
 <template>
-    
+   <span>
+     组件开发中，敬请期待 <img src="/loading.gif" alt=""></img>
+   </span>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style scoped>
+span {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    font-weight: bold;
+    color:var(--topic-color-text)
+}
+
+img {
+    width: 100px;
+    height: 100px;
+    object-fit: contain;
+}
 </style>
